e355bdff149c21472248b5882f18786fe9c322ab
Pournima <pournimawohlig@gmail.com>
2021-10-14 12:42:40 +0530

    olympics

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.post("/mens", async (req, res) => {
 });
 
 // we will handle get req
-app.get("/mens", async (req, res) => {
+app.get("/mens/00", async (req, res) => {
     try {
         const getMens = await MensRanking.find({});
         res.send(getMens);
@@ -31,24 +31,38 @@ app.get("/mens", async (req, res) => {
 });
 
 // we will handle get req of indiviual
-app.get("/mens/:id", async (req, res) => {
+app.get("/mens", async (req, res) => {
     try {
-        const _id = req.params.id;
-        const getMen = await MensRanking.findById({ _id });
+        const getMen = await MensRanking.find({}).sort({ ranking: 1 });
+
         res.send(getMen);
     } catch (e) {
         res.status(400).send(e);
     }
 });
+//Blah.find({}).sort({date: -1}).execFind(function(err,docs){
+// aggregate.sort({ field: 'asc', test: -1 });
 
 // we will handle patch req of indiviual
 app.patch("/mens/:id", async (req, res) => {
     try {
         const _id = req.params.id;
-        const getMen = await MensRanking.findByIdAndUpdate({ _id });
+        const getMen = await MensRanking.findByIdAndUpdate(_id, req.body, {
+            new: true,
+        });
         res.send(getMen);
     } catch (e) {
-        res.status(400).send(e);
+        res.status(500).send(e);
+    }
+});
+
+// we will handle delete req of indiviual
+app.delete("/mens/:id", async (req, res) => {
+    try {
+        const getMen = await MensRanking.findByIdAndDelete(req.params.id);
+        res.send(getMen);
+    } catch (e) {
+        res.status(500).send(e);
     }
 });
 
